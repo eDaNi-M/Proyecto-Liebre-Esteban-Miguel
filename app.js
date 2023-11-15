0003cbfb6234933a2f8d33030707e05a812c6922
eDaNi-M <danimiguel9626@gmail.com>
2023-11-15 10:33:43 -0300

    modificaciones generales

--- app.js
+++ app.js
@@ -7,15 +7,14 @@ app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, '/views/home.html'));
 });
 
-// app.get("/index", (req, res) => {
-//     res.sendFile(path.join(__dirname, '/views/index.html'));
-// });
 
-
-
-// const port = 3010;
-
-const port = process.env.PORT || 3001
+app.get("/register", (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/registro.html'))
+})
+app.get("/login", (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/login.html'))
+})
+const port = process.env.PORT || 3070
 app.listen(port, () => {
     console.log(`Servidor habilitado http://localhost:${port}`);
 });
